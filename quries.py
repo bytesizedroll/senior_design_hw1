1207b8d17cf446ef55a2a4a331767c32e2bc6a57
John Spencer <johnspencer15@gmail.com>
2016-10-17 13:49:13 -0400

    Add two queries to test with

---
+++ quries.py
@@ -0,0 +1,4 @@
+queries = {
+    'is it possible to increase the jazz audience': 'http://www.npr.org/sections/ablogsupreme/2012/05/23/153461410/it-cant-be-done-the-difficulty-of-growing-a-jazz-audience',
+    'setup jupyterhub on server': 'https://github.com/jupyterhub/jupyterhub/wiki/Installation-of-Jupyterhub-on-remote-server'
+}
